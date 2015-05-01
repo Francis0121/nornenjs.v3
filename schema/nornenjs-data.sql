47c3fbffff98d2c3be7c638fb3d71c4be76d88c2
Francis Kim <kims_0121@naver.com>
2015-05-01 19:21:19 +0900

    DB Schema

--- schema/nornenjs-data.sql
+++ schema/nornenjs-data.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v11.01 (64 bit)
-MySQL - 5.5.28 : Database - nornenjs
+MySQL - 5.5.41-0ubuntu0.12.04.1 : Database - nornenjs
 *********************************************************************
 */
 
@@ -27,13 +27,11 @@ CREATE TABLE `actor` (
   `enabled` tinyint(1) NOT NULL,
   PRIMARY KEY (`pn`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `actor` */
 
 LOCK TABLES `actor` WRITE;
-
-insert  into `actor`(`pn`,`username`,`password`,`enabled`) values (8,'nornenjs','f981fbf4314e1c077fc9f559c60c6272a1f95b13',1),(9,'teriusbin','6b783b11b13bced02be9fa442c045975c75c746b',1),(10,'eikids','63f5d6d8974334bd465e06899433eeffb6e85043',1),(11,'dsa12','a6e714a58c5f18e91d0043ebff1c152c0a405d48',1);
 
 UNLOCK TABLES;
 
@@ -55,8 +53,6 @@ CREATE TABLE `actor_info` (
 /*Data for the table `actor_info` */
 
 LOCK TABLES `actor_info` WRITE;
-
-insert  into `actor_info`(`actorPn`,`email`,`firstName`,`lastName`,`inputDate`,`updateDate`) values (8,'nornenjs@naver.com','Francis','Kim','2015-04-29 22:24:58','2015-04-30 03:10:45'),(9,'teriusbin@naver.com','Woo','Lee','2015-04-29 23:07:11','2015-04-29 23:07:11'),(10,'eikids@naver.com','Francis','Kim','2015-04-29 23:11:13','2015-04-30 02:47:11'),(11,'ds@naver.com','sdasda','kims','2015-04-29 23:13:07','2015-04-29 23:13:07');
 
 UNLOCK TABLES;
 
@@ -88,7 +84,7 @@ CREATE TABLE `board` (
   `insertDate` datetime NOT NULL COMMENT 'Board insert date',
   `updateDate` datetime NOT NULL COMMENT 'Board update date',
   PRIMARY KEY (`pn`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `board` */
 
@@ -110,13 +106,11 @@ CREATE TABLE `data` (
   PRIMARY KEY (`pn`),
   KEY `username` (`username`),
   CONSTRAINT `data_ibfk_1` FOREIGN KEY (`username`) REFERENCES `actor` (`username`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=230 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `data` */
 
 LOCK TABLES `data` WRITE;
-
-insert  into `data`(`pn`,`username`,`type`,`name`,`savePath`,`inputDate`) values (71,'nornenjs',1,'temp.den','${data.rootUploadPath\\nornenjs\\908ba9e8-0dbd-42ec-9cf9-5bc301721045','2015-04-30 08:57:15'),(72,'nornenjs',1,'temp.den','\"E:\\nornenjs\"\\nornenjs\\62207e57-4d0b-4d3c-8999-771ed6af672a','2015-04-30 08:58:24'),(73,'nornenjs',1,'temp.den','\"E:\\nornenjs\"\\nornenjs\\19a0dd0a-bae0-4379-b43b-3485f435e019','2015-04-30 08:58:58'),(74,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\51a564ea-89aa-4330-9ed8-c1cb741f6146','2015-04-30 09:01:02'),(75,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\9bd002a3-3be9-4b09-b946-928752b01d63','2015-04-30 09:03:23'),(76,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\3b1be3e1-a5d3-4969-88bc-7d8b68506be7','2015-04-30 09:08:15'),(77,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\4a25b46d-873f-4211-a1b0-c92515506688','2015-04-30 09:08:46'),(78,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\a8ed6d5d-cae0-4428-9a16-5af3bf1bb52c','2015-04-30 09:12:52'),(79,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\f80b8089-ff09-4219-96e0-5442ffbda2a4','2015-04-30 09:13:57'),(80,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\4809fa55-a4f3-418e-b9bd-71b965d92b22','2015-04-30 09:14:43'),(81,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\4a7b3b63-9bdb-426c-ab4d-b41efa2146b3','2015-04-30 09:15:46'),(82,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\d3bab178-20f4-42f8-94e7-ccc26392ff00','2015-04-30 09:16:03'),(83,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\86a4b8ae-0a33-49f4-a4bc-68666bbd883b','2015-04-30 09:16:13'),(84,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\edc3dd00-8241-4c15-ac21-5bcdece60ece','2015-04-30 09:16:41'),(85,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\9aba8adf-b74e-4209-85d6-7e052ef89b86','2015-04-30 09:17:24'),(86,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\a4e628bc-60da-441c-b934-e5f77cafc2b5','2015-04-30 09:17:33'),(87,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\e1d835a9-a053-4edd-96ac-a9dead82fe25','2015-04-30 09:17:40'),(88,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\21021af0-f431-4e03-91fe-0080d316849c','2015-04-30 09:18:02'),(89,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\d9898024-1578-45aa-a96e-976e3a34937d','2015-04-30 09:18:34'),(90,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\9f48d924-9e64-4c67-9e7f-d43c5676f3db','2015-04-30 09:19:26'),(219,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\61ac7271-ec4f-4d48-b4c5-d50cb57d1efc','2015-04-30 09:31:49'),(220,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\09c914fa-1e92-48bb-9917-2a233a7c408e','2015-04-30 09:36:29'),(221,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\dcf55612-2835-498d-b04c-889561494d5d','2015-04-30 09:36:54'),(222,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\498991b8-cba1-49eb-a732-510b34b0ec0e','2015-04-30 09:37:05'),(223,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\1d6f5223-b49a-4e11-a9f6-11df55207b21','2015-04-30 09:38:06'),(224,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\eecd5b43-3f64-49b1-ad66-dd15ae3b1ac1','2015-04-30 09:38:40'),(225,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\205756ec-5ab0-483d-8e7d-6f53eee960ce','2015-04-30 09:39:10'),(226,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\4b6ea09d-4294-47da-8437-ce726ee93ae9','2015-04-30 09:39:23'),(227,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\1dcb2b0e-9be3-4ea7-a372-d03c9b2ed7cc','2015-04-30 09:40:16'),(228,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\2a3b42db-508c-4149-bcfe-45277dccd680','2015-04-30 09:41:59'),(229,'nornenjs',1,'temp.den','E:\\nornenjs\\data\\b012a05e-3970-4fee-ad66-f516df05def6','2015-04-30 09:49:16');
 
 UNLOCK TABLES;
 
@@ -152,13 +146,11 @@ CREATE TABLE `group_members` (
   KEY `group_members_ibfk_1` (`groupPn`),
   CONSTRAINT `group_members_ibfk_1` FOREIGN KEY (`groupPn`) REFERENCES `groups` (`pn`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `group_members_ibfk_2` FOREIGN KEY (`username`) REFERENCES `actor` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `group_members` */
 
 LOCK TABLES `group_members` WRITE;
-
-insert  into `group_members`(`pn`,`username`,`groupPn`) values (1,'nornenjs',1),(2,'teriusbin',2),(3,'eikids',2),(4,'dsa12',2);
 
 UNLOCK TABLES;
 
@@ -170,7 +162,7 @@ CREATE TABLE `groups` (
   `pn` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `groupName` varchar(50) NOT NULL,
   PRIMARY KEY (`pn`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `groups` */
 
@@ -198,6 +190,53 @@ LOCK TABLES `persistent_logins` WRITE;
 
 UNLOCK TABLES;
 
+/*Table structure for table `thumbnail` */
+
+DROP TABLE IF EXISTS `thumbnail`;
+
+CREATE TABLE `thumbnail` (
+  `dataPn` bigint(20) NOT NULL,
+  `thumbnailPn` bigint(20) NOT NULL,
+  PRIMARY KEY (`dataPn`,`thumbnailPn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `thumbnail` */
+
+LOCK TABLES `thumbnail` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `thumbnail_option` */
+
+DROP TABLE IF EXISTS `thumbnail_option`;
+
+CREATE TABLE `thumbnail_option` (
+  `pn` int(11) NOT NULL AUTO_INCREMENT,
+  `width` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `mprType` tinyint(1) NOT NULL COMMENT 'MPR Type',
+  `quality` tinyint(1) NOT NULL COMMENT 'Quality',
+  `brightness` float NOT NULL,
+  `density` float NOT NULL,
+  `transferOffset` float NOT NULL,
+  `transferScaleX` float NOT NULL,
+  `transferScaleY` float NOT NULL,
+  `transferScaleZ` float NOT NULL,
+  `positionZ` float NOT NULL,
+  `rotationX` float NOT NULL,
+  `rotationY` float NOT NULL,
+  PRIMARY KEY (`pn`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `thumbnail_option` */
+
+LOCK TABLES `thumbnail_option` WRITE;
+
+insert  into `thumbnail_option`(`pn`,`width`,`height`,`type`,`mprType`,`quality`,`brightness`,`density`,`transferOffset`,`transferScaleX`,`transferScaleY`,`transferScaleZ`,`positionZ`,`rotationX`,`rotationY`) values (1,512,512,1,0,0,1,0.05,0,0,0,0,3,0,0),(2,512,512,3,1,0,1,0.05,0,0.5,0,0,3,0,0),(3,512,512,3,2,0,1,0.05,0,0,0.5,0,3,0,0),(4,512,512,3,3,0,1,0.05,0,0,0,0.5,3,0,0);
+
+UNLOCK TABLES;
+
 /*Table structure for table `volume` */
 
 DROP TABLE IF EXISTS `volume`;
@@ -215,13 +254,11 @@ CREATE TABLE `volume` (
   UNIQUE KEY `volumeDataPn` (`volumeDataPn`),
   KEY `username` (`username`),
   CONSTRAINT `volume_ibfk_1` FOREIGN KEY (`username`) REFERENCES `actor` (`username`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=185 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 /*Data for the table `volume` */
 
 LOCK TABLES `volume` WRITE;
-
-insert  into `volume`(`pn`,`username`,`volumeDataPn`,`title`,`width`,`height`,`depth`,`inputDate`) values (181,'nornenjs',227,'25',100,100,100,'2015-04-30 09:40:30'),(182,'nornenjs',228,'설명',120,120,100,'2015-04-30 09:42:07'),(183,'nornenjs',229,'설명23',202,202,102,'2015-04-30 09:50:41');
 
 UNLOCK TABLES;
 
---
+++ schema/thumbnail.sql
@@ -0,0 +1,70 @@
+/*
+SQLyog Ultimate v11.01 (64 bit)
+MySQL - 5.5.41-0ubuntu0.12.04.1 : Database - nornenjs
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`nornenjs` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `nornenjs`;
+
+/*Table structure for table `thumbnail` */
+
+DROP TABLE IF EXISTS `thumbnail`;
+
+CREATE TABLE `thumbnail` (
+  `dataPn` bigint(20) NOT NULL,
+  `thumbnailPn` tinyint(11) NOT NULL,
+  PRIMARY KEY (`dataPn`,`thumbnailPn`),
+  CONSTRAINT `thumbnail_ibfk_2` FOREIGN KEY (`dataPn`) REFERENCES `data` (`pn`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `thumbnail` */
+
+LOCK TABLES `thumbnail` WRITE;
+
+UNLOCK TABLES;
+
+/*Table structure for table `thumbnail_option` */
+
+DROP TABLE IF EXISTS `thumbnail_option`;
+
+CREATE TABLE `thumbnail_option` (
+  `pn` int(11) NOT NULL AUTO_INCREMENT,
+  `width` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `mprType` tinyint(1) NOT NULL COMMENT 'MPR Type',
+  `quality` tinyint(1) NOT NULL COMMENT 'Quality',
+  `brightness` float NOT NULL,
+  `density` float NOT NULL,
+  `transferOffset` float NOT NULL,
+  `transferScaleX` float NOT NULL,
+  `transferScaleY` float NOT NULL,
+  `transferScaleZ` float NOT NULL,
+  `positionZ` float NOT NULL,
+  `rotationX` float NOT NULL,
+  `rotationY` float NOT NULL,
+  PRIMARY KEY (`pn`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `thumbnail_option` */
+
+LOCK TABLES `thumbnail_option` WRITE;
+
+insert  into `thumbnail_option`(`pn`,`width`,`height`,`type`,`mprType`,`quality`,`brightness`,`density`,`transferOffset`,`transferScaleX`,`transferScaleY`,`transferScaleZ`,`positionZ`,`rotationX`,`rotationY`) values (1,512,512,1,0,0,1,0.05,0,0,0,0,3,0,0),(2,512,512,3,1,0,1,0.05,0,0,0,0,3,0,0),(3,512,512,3,2,0,1,0.05,0,0,0,0,3,0,0),(4,512,512,3,3,0,1,0.05,0,0,0,0,3,0,0);
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
